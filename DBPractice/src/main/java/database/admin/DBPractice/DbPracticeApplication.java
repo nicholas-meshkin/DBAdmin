ebb34e9941106b1a4471159ab20c6e20c8030766
nicholas_meshkin <nick.t.meshkin@gmail.com>
2019-05-15 16:23:48 -0400

    first commit for database admin practice project

---
+++ DBPractice/src/main/java/database/admin/DBPractice/DbPracticeApplication.java
@@ -0,0 +1,13 @@
+package database.admin.DBPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbPracticeApplication.class, args);
+	}
+
+}
---
+++ DBPractice/src/main/java/database/admin/DBPractice/MainController.java
@@ -0,0 +1,17 @@
+package database.admin.DBPractice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("index");
+		
+		return mav;
+		
+	}
+}
